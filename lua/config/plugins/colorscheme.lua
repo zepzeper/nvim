a60a30d3126d30ada65d647a1d080023d654e4f1
wouter <wouterschiedam98@gmail.com>
2025-05-29 19:33:59 +0200

    broken

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,12 +1,13 @@
+-- lua/plugins/rose-pine.lua
 return {
-    "drewxs/ash.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("ash").setup({
-          transparent = true,
-          term_colors = true,
-        })
-        vim.cmd([[colorscheme ash]])
-    end
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+    require("rose-pine").setup({
+      styles = {
+        transparency = true,
+      },
+    })
+		vim.cmd("colorscheme rose-pine")
+	end
 }
