e721092ee047ae4f51819a2d302fef55c2589732
Wouter van Krugten <wouterschiedam98@gmail.com>
2025-10-21 19:38:01 +0200

    colorsmypencils

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,14 +1,7 @@
-
 return {
-  'projekt0n/github-nvim-theme',
-  name = 'github-theme',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require('github-theme').setup({
-    })
-    
-    -- Use github_dark instead of github_dark_dimmed for better contrast
-    vim.cmd('colorscheme github_dark')
-  end,
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		vim.cmd("colorscheme rose-pine")
+	end
 }
