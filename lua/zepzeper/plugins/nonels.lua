9e4aa7393fbfc51845499dade194860d82c4d13f
Wouter van Krugten <wouter.vankrugten@procurios.nl>
2026-05-28 13:27:42 +0200

    Write to /tmp instead of git dir

--- lua/zepzeper/plugins/nonels.lua
+++ lua/zepzeper/plugins/nonels.lua
@@ -1,6 +1,9 @@
 local null_ls = require("null-ls")
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
+    temp_dir = "/tmp",
     sources = {
         null_ls.builtins.diagnostics.phpstan.with({
             command = "vendor/bin/phpstan",
@@ -14,16 +17,20 @@ null_ls.setup({
     on_attach = function(client, bufnr)
         if client:supports_method("textDocument/formatting") then
             vim.api.nvim_clear_autocmds({
+                group = augroup,
                 buffer = bufnr,
-                event = "BufWritePre",
             })
 
             vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
                 buffer = bufnr,
                 callback = function()
                     vim.lsp.buf.format({
                         bufnr = bufnr,
                         timeout_ms = 2000,
+                        filter = function(c)
+                            return c.name == "null-ls"
+                        end,
                     })
                 end,
             })
