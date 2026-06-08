b51b6a1dad33c886ed38f63aca2153af3dbae175
Wouter van Krugten <wouter.vankrugten@procurios.nl>
2026-06-08 15:59:15 +0200

    TS & theme

--- lua/zepzeper/loader/plugins.lua
+++ lua/zepzeper/loader/plugins.lua
@@ -150,6 +150,7 @@ return {
             vim.cmd("colorscheme rose-pine")
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
         end
     },
     -- git
--- lua/zepzeper/native/autocmd.lua
+++ lua/zepzeper/native/autocmd.lua
@@ -1,18 +1,18 @@
 local group = vim.api.nvim_create_augroup("BlameStackTS", { clear = true })
 
-vim.api.nvim_create_autocmd("BufWinEnter", {
-    group = group,
-    callback = function(ev)
-        local name = vim.api.nvim_buf_get_name(ev.buf)
-        if name:find("Blame stack", 1, true) then
-            local ft = vim.bo[ev.buf].filetype
-            if ft ~= "" then
-                local ok, err = pcall(vim.treesitter.start, ev.buf, ft)
-                vim.notify(ok and "TS started" or "TS error: " .. err)
-            end
-        end
-    end,
-})
+-- vim.api.nvim_create_autocmd("BufWinEnter", {
+--     group = group,
+--     callback = function(ev)
+--         local name = vim.api.nvim_buf_get_name(ev.buf)
+--         if name:find("Blame stack", 1, true) then
+--             local ft = vim.bo[ev.buf].filetype
+--             if ft ~= "" then
+--                 local ok, err = pcall(vim.treesitter.start, ev.buf, ft)
+--                 vim.notify(ok and "TS started" or "TS error: " .. err)
+--             end
+--         end
+--     end,
+-- })
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "dap-view", "dap-repl" },
@@ -20,4 +20,15 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.wo.number = true
         vim.wo.relativenumber = true
     end,
+})
+
+
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(ev)
+    -- always attach for real languages, even in diff buffers
+    if ev.match == "" then return end
+
+    pcall(vim.treesitter.start, ev.buf, ev.match)
+  end,
 })
