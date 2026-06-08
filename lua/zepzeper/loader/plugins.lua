41453149a7160a6bbef31eccb9071a928400182f
Wouter van Krugten <wouter.vankrugten@procurios.nl>
2026-06-08 09:21:29 +0200

    visql

--- lua/zepzeper/loader/plugins.lua
+++ lua/zepzeper/loader/plugins.lua
@@ -214,6 +214,20 @@ return {
     --     end,
     -- },
     {
+        "kopecmaciej/vi-sql.nvim",
+        config = function()
+            require("vi-sql").setup({
+                -- Press this inside vi-sql to hide the window (change to taste)
+                hide_key = "<C-q>",
+            })
+        end,
+        cmd = { "ViSQL", "ViSQLJump" },
+        keys = {
+            { "<leader>vs", "<cmd>ViSQL<cr>", desc = "Open vi-sql" },
+            -- { "<leader>vj", ":ViSQLJump ", desc = "vi-sql: jump to table", silent = false },
+        },
+    },
+    {
         'MeanderingProgrammer/render-markdown.nvim',
         opts = {},
         config = function()
