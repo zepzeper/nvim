782af35d4a7c07e532e2ec644af0c084a5f04a8b
Wouter van Krugten <wouter.vankrugten@procurios.nl>
2026-09-01 10:40:06 +0200

    automated dev commit

---
+++ lua/plugins/compile.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "ej-shafran/compile-mode.nvim",
+    version = "^5.0.0",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "m00qek/baleia.nvim", tag = "v1.3.0" },
+    },
+    config = function()
+      ---@type CompileModeOpts
+      vim.g.compile_mode = {
+        -- if you use something like `nvim-cmp` or `blink.cmp` for completion,
+        -- set this to fix tab completion in command mode:
+         input_word_completion = true,
+
+        -- to add ANSI escape code support, add:
+         baleia_setup = true,
+
+        -- to make `:Compile` replace special characters (e.g. `%`) in
+        -- the command (and behave more like `:!`), add:
+         bang_expansion = true,
+      }
+    end
+  }
+}
