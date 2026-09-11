07a1d2fbeeb117f33cbf7107626c416617905622
Wouter van Krugten <wouter.vankrugten@procurios.nl>
2026-09-11 11:33:57 +0200

    automated dev commit

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -582,14 +582,14 @@ return {
     priority = 1000,
     config = function()
       require("nordic").setup({
-        bold_keywords = false,
+        bold_keywords = true,
         italic_comments = false,
         transparent = { bg = true, float = true },
-        bright_border = false,
-        reduced_blue = true,
+        bright_border = true,
+        reduced_blue = false,
         swap_backgrounds = false,
         cursorline = {
-          bold = false,
+          bold = true,
           bold_number = true,
           theme = "dark",
           blend = 0.85,
